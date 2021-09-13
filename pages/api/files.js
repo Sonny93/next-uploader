eadd77d6a13f8e66fc6e98c2246771808ea55841
Sonny <sonnyasdev@gmail.com>
2021-09-13 10:55:37 +0200

    upload finish

--- pages/api/files.js
+++ pages/api/files.js
@@ -2,11 +2,8 @@ import { readdir } from 'fs/promises';
 
 export default async function handler(req, res) {
     try {
-        const files = await readdir('./public/uploads');
-        for (const file of files) {
-            console.log(file);
-        }
-        res.status(200).json({ count: files.length });
+        const files = await (await readdir('./public/static/uploads')).map((file) => `./static/uploads/${file}`);
+        res.status(200).json({ files: Array.from(files) });
     } catch (error) {
         res.status(501).json({ error });
     }
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,78 @@
 import { useSession, signIn, signOut } from "next-auth/client";
-import { useState } from "react";
-import { readdir } from 'fs/promises';
-
-const files = [];
-readdir('./public/uploads').then((file) => files.push(require(file)));
+import { useEffect, useState } from "react";
+import toastr from "toastr";
+import "toastr/build/toastr.css";
 
 export default function Home() {
 	const [session, loadingSession] = useSession();
 	const [filesUpload, setFilesUpload] = useState(null);
+	const [files, setFiles] = useState(null);
 
-	if (!session) {
-		return <div className='App'>
-			Vous n'êtes pas connecté <button onClick={() => signIn()}>Se connecter</button>
-		</div>
-	}
+	useEffect(async () => {
+		const request = await fetch('/api/files');
+		if (!request.ok)
+			return console.error(request);
+
+		const data = await request.json();
+		setFiles(data?.files || []);
+	}, []);
 
 	async function upload() {
 		if (!filesUpload || filesUpload?.length < 1) return;
 
 		const formData = new FormData();
 		Array.from(filesUpload).forEach((file) => formData.append('theFiles', file));
-		console.log(filesUpload);
 
-		const request = await fetch('/api/upload', { 
+		const request = await fetch('/api/upload', {
 			method: 'post',
 			body: formData
 		});
+		
 		const data = await request.json();
-		console.log(data);
 		setFilesUpload(null);
+		
+		setFiles(data?.files ? (filesPrev) => {
+			const filesToReturn = Array.from(filesPrev);
+			for (const file of data?.files) 
+				filesToReturn.push(file);
+				
+			return filesToReturn;
+		} : []);
 	}
 
+	function Navbar() {
+		if (!session) {
+			return <div className='navbar'>
+				Vous n'êtes pas connecté <button onClick={() => signIn()}>Se connecter</button>
+			</div>;
+		} else {
+			return <div className='navbar'>
+				Vous êtes connecté <button onClick={() => signOut()}>Se déconnecter</button>
+			</div>;
+		}
+	}
+
+	const Uploader = () => {
+		return <>
+			<input type="file" onChange={(event) => setFilesUpload(event.target.files)} multiple={true} />
+			<button onClick={upload}>
+				Upload
+			</button>
+		</>;
+	}
+
+	console.log('files', files);
 	return <div className='App'>
-		Vous êtes connecté <button onClick={() => signOut()}>Se déconnecter</button>
-		<input type="file" onChange={(event) => setFilesUpload(event.target.files)} multiple={true} />
-		<button onClick={upload}>
-			Upload
-		</button>
-			{files && files?.length < 1 ? <>
-				{files.map((file, key) => <img key={key} src={file} />)}
-			</> : 'no files'}
+		<div className='header'>
+			<Navbar />
+			{session && <Uploader />}
+		</div>
+		<ul className='images'>
+			{files && files?.length >= 1 ? <>
+				{files.map((file, key) => <li className='image-container' key={key}>
+					<img src={file} />
+				</li>)}
+			</> : 'Aucun fichier'}
+		</ul>
 	</div>
 }
