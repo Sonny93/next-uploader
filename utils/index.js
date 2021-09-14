026e9f1c4c4ae00e0e874a78f6288352eed96b19
Sonny <sonnyasdev@gmail.com>
2021-09-14 16:47:10 +0200

    add class File

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 function calculSize(size) {
 	const units = ['Ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo'];
 	let toReturn;
@@ -8,4 +10,35 @@ function calculSize(size) {
 	return toReturn;
 }
 
-export { calculSize };
+function fileType(extension) {
+	const images = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'ps', 'psd', 'svg', 'tif', 'tiff'];
+	const audios = ['aif', 'cda', 'mid', 'midi', 'mp3', 'mpa', 'ogg', 'wav', 'wpl'];
+	const videos = ['3g2', '3gp', 'avi', 'h264', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv'];
+
+	if (images.includes(extension)) {
+		return 'image';
+	} else if (audios.includes(extension)) {
+		return 'audio';
+	} else if (videos.includes(extension)) {
+		return 'video';
+	} else {
+		return 'other';
+	}
+}
+
+class File {
+    constructor({ fileName, size, uploadPath }) {
+        const extension = (path.extname(fileName)).substring(1);
+
+        this.name = fileName.substring(0, fileName.length - extension.length);
+        this.extension = extension;
+        this.type = fileType(extension);
+
+        this.fileName = fileName;
+        this.brutSize = size;
+        this.size = calculSize(size);
+        this.uploadPath = `${uploadPath}/${fileName}`;
+    }
+}
+
+export { calculSize, fileType, File };
