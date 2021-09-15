82169a7d355dbeedefe6e0005b82241d95bf402a
Sonny <sonny.lepapa@gmail.com>
2021-09-15 22:54:52 +0200

    improve upload modal

--- components/Upload.js
+++ components/Upload.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Modal from 'react-modal';
 import toastr from 'toastr';
 
+import { AiFillFileAdd } from 'react-icons/ai';
+
 import { calculSize } from '../utils';
 
 function FileInput({ refInput, setFiles }) {
@@ -49,33 +51,41 @@ export default function Upload({ setFiles, filesUpload, setFilesUpload, isBrowse
 	}
 
     function ModalFilesUpload() {
+        console.log(filesUpload);
         return <Modal 
             isOpen={true} 
-            contentLabel="Files list" 
-            appElement={isBrowser ? document.getElementById("__next") : document.body}>
-            {filesUpload && Array.from(filesUpload).map((file, key) => {
-                return <li key={key}>
-                    {file.name} - {file.size}
-                </li>
-            })}
-            <button onClick={upload} disabled={false}>
-                Envoyer ({filesUpload.length} fichier{filesUpload.length > 1 ? 's' : ''})
+            contentLabel="File list" 
+            appElement={isBrowser ? document.getElementById("__next") : document.body}
+            className='modal-container'>
+            <button onClick={() => setFilePreview(null)} className='close'>
+                fermer
             </button>
-            <button onClick={cancelUpload} disabled={false}>
-                Annuler
-            </button>
+            <div className='modal-content'>
+                {filesUpload && Array.from(filesUpload).map((file, key) => {
+                    return <li key={key}>
+                        {file.name} - {file.size}
+                    </li>
+                })}
+                <button onClick={upload} disabled={false}>
+                    Envoyer ({filesUpload.length} fichier{filesUpload.length > 1 ? 's' : ''})
+                </button>
+                <button onClick={cancelUpload} disabled={false}>
+                    Annuler
+                </button>
+            </div>
         </Modal>
     }
 
     return <div className='uploader'>
         {filesUpload?.length > 0 ? <ModalFilesUpload /> : null}
-        <button
+        <div
+            className='icon-btn'
             onClick={(e) => {
                 e.preventDefault();
                 refInputFile.current.click();
             }}>
-            +
-        </button>
+            <AiFillFileAdd />
+        </div>
         <FileInput refInput={refInputFile} setFiles={setFilesUpload} />
     </div>;
 }
