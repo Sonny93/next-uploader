aa70179cf0f2b13cfdf1b12d8ce1b4cb9db91113
Sonny <sonny.lepapa@gmail.com>
2021-09-15 22:54:15 +0200

    update style files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css';
 import 'toastr/build/toastr.css';
+import '../styles/globals.css';
+import '../styles/modal.css';
 import '../styles/filelist.css';
 import '../styles/header.css';
+import '../styles/upload.css';
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
