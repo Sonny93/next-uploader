76da79154513b938acf1759900960f6a5000b340
Sonny <sonnyasdev@gmail.com>
2021-10-18 02:54:32 +0200

    ajout upload db file prisma via multer

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,28 +1,23 @@
-import { stat } from 'fs/promises';
 import nextConnect from 'next-connect';
-import {  getSession } from 'next-auth/client';
+import { getSession } from 'next-auth/client';
 import multer from 'multer';
+import path from 'path';
 
-import { File } from '../../utils';
+import { calculSize } from '../../utils';
 
-const upload = multer({
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const upload = multer({ 
     storage: multer.diskStorage({
         destination: process.env.UPLOAD_DIR,
-        filename: (req, file, cb) => {
-            // faire les verifs ici avant d'upload
-            console.log('là', file);
-            cb(null, file?.originalname);
-        }
+        filename: async (req, file, cb) => cb(null, file.originalname)
     })
 });
 
 const apiRoute = nextConnect({
-    onError(error, req, res) {
-        res.status(501).json({ error: `Une erreur est survenue! ${error.message}` });
-    },
-    onNoMatch(req, res) {
-        res.status(405).json({ error: `La méthode '${req.method}' n'est pas autorisée` });
-    },
+    onError: (error, req, res) => res.status(501).json({ error: `Une erreur est survenue! ${error.message}` }),
+    onNoMatch: (req, res) => res.status(405).json({ error: `La méthode '${req.method}' n'est pas autorisée` })
 });
 
 apiRoute.use(async (req, res, next) => { // Middleware auth
@@ -33,22 +28,41 @@ apiRoute.use(async (req, res, next) => { // Middleware auth
         next();
 });
 
-// Check for upload
 apiRoute.use(upload.single('file'));
-
-// Post file upload
-apiRoute.post(async (req, res) => {
-    const fileReq = req?.file;
-    const fileStat = await (await stat(`${process.env.UPLOAD_DIR}/${fileReq?.originalname}`));
-    const file = new File({ fileName: fileReq?.originalname, size: fileStat.size, url: process.env.UPLOAD_URL });
+apiRoute.use(async (req, res) => {
+    console.log('là', req.file);
+    const customName = req.body?.customName;
+    const password = req.body?.password;
     
-    res.status(200).json({ file });
+    const file = req.file;
+
+    const extension = path.extname(req.file.originalname);
+    const fileName = (customName || file.originalname);
+
+    try {
+        const fileDB = await prisma.file.create({
+            data: {
+                name: customName,
+                fullname: fileName + extension,
+                password: password,
+                passwordSet: password ? true : false,
+                size: calculSize(file.size),
+                uploadUrl: `${process.env.UPLOAD_URL}/${file.filename}`,
+                uploadPath: file.path,
+                fileBrutSize: file.size,
+                fileExtension: extension,
+                fileKind: file.mimetype
+            }
+        });
+        console.log(fileDB);
+        res.status(200).send({ status: 'OK' });
+    } catch (error) {
+        console.error(error);
+        res.status(403).send({ error: 'Une erreur est survenue' });
+    }
 });
 
 export default apiRoute;
-
 export const config = {
-    api: {
-        bodyParser: false
-    }
+    api: { bodyParser: false }
 }
