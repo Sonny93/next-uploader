e414f97d107879841251feccbd679f4daccf484d
Sonny <sonny.lepapa@gmail.com>
2021-10-19 17:26:37 +0200

    utils -> remove code useless + création fonction pour return les infos non sensibles fichiers

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,3 @@
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-
 function calculSize(value = 0, decimals = 2) {
     const octets = Number(value);
 	if (octets === 0) return '0 Octet';
@@ -14,36 +11,16 @@ function calculSize(value = 0, decimals = 2) {
     return parseFloat((octets / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
-function fileType(extension) {
-	const images = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'ps', 'psd', 'svg', 'tif', 'tiff'];
-	const audios = ['aif', 'cda', 'mid', 'midi', 'mp3', 'mpa', 'ogg', 'wav', 'wpl'];
-	const videos = ['3g2', '3gp', 'avi', 'h264', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm', 'swf', 'vob', 'wmv'];
-
-	if (images.includes(extension)) {
-		return 'image';
-	} else if (audios.includes(extension)) {
-		return 'audio';
-	} else if (videos.includes(extension)) {
-		return 'video';
-	} else {
-		return 'other';
-	}
+function fileSafeProps(file) {
+    file.size = calculSize(file.fileBrutSize);
+    file.url = `${process.env.UPLOAD_URL}/${file.file_id}`;
+    
+    delete file.password;
+    delete file.id;
+    // delete file.fileBrutSize;
+    delete file.fileSaveAs;
+    
+    return file;
 }
 
-class File {
-    constructor({ id, fileName, size, url }) {
-        const extension = (path.extname(fileName)).substring(1);
-
-		this.id = id || uuidv4();
-        this.name = fileName.substring(0, fileName.length - extension.length - 1);
-        this.extension = extension;
-        this.type = fileType(extension);
-
-        this.fileName = fileName;
-        this.brutSize = size;
-        this.size = calculSize(size);
-        this.url = `${url}/${fileName}`;
-    }
-}
-
-export { calculSize, fileType, File };
+export { calculSize, fileSafeProps };
