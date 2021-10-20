68ac13ac0bb5bf0fb3e985057b85e37a2f3a6f31
Sonny <sonny.lepapa@gmail.com>
2021-10-20 14:18:35 +0200

    fix metas -> réécriture du composant meta

--- pages/file/[fid].js
+++ pages/file/[fid].js
@@ -1,7 +1,8 @@
 import Link from 'next/link';
-import Head from 'next/head';
 
-import { calculSize, fileSafeProps } from '../../utils';
+import { fileSafeProps } from '../../utils';
+
+import Meta from '../../components/Meta';
 import FilePreview from '../../components/FilePreview';
 
 import { PrismaClient } from '@prisma/client';
@@ -10,40 +11,19 @@ const prisma = new PrismaClient();
 BigInt.prototype.toJSON = function () { return this.toString() }
 
 export default function File({ fid, file, error }) {
-    function Meta() {
-        return <>
-            <Head>
-                <meta property='og:author' content='Sonny#0005' />
-                <meta property='og:site_name' content='🟣 >_ Uploader.sonnydata.fr' />
-                <meta charSet='UTF-8' />
-                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-                <meta name='theme-color' content='#fff' />
-
-                <meta property='og:url' content='/file/20210227_062940.jpg/' />
-                <meta property='og:type' content='website' />
-
-                <title>
-                    Uploader {`• ${file ? file.name : fid}`}
-                </title>
-
-                {!file ? <>
-                    <title>Uploader • {fid}</title>
-                    <meta property='og:title' content={`Uploader • ${fid}`} />
-                    <meta property='og:description' content='Uploader sonnydata.fr • Fichier introuvable' />
-                </> : <>
-                    <title>Uploader • {file.name}</title>
-                    <meta property='og:title' content={`Uploader • ${file.name}`} />
-                    <meta property='og:description' content='Uploader sonnydata.fr' />
-                    <meta property='og:image' content={file.url} />
-                </>}
-            </Head>
-        </>;
-    }
-
-    console.log(file);
     return (
         <div className='App'>
-            <Meta />
+            {!file ? 
+                <Meta 
+                    title={`Uploader • ${fid}`} 
+                    description='• Fichier introuvable' /> :
+                <Meta 
+                    title={`Uploader • ${file.name}`} 
+                    description={file.name}
+                    pageUrl={`${process.env.NEXTAUTH_URL}/file/${file.file_id}`} 
+                    assetUrl={file.url} />
+            }
+
             <header style={{ justifyContent: 'center' }}>
                 <Link href='/'>
                     <a className='home-link'>Revenir à la page d'accueil</a>
@@ -64,7 +44,6 @@ export default function File({ fid, file, error }) {
 
 export async function getServerSideProps({ query }) {
     const { fid } = query;
-    console.log('fid', fid);
     
     const file = await prisma.file.findUnique({
         where: { file_id: fid }
@@ -72,9 +51,9 @@ export async function getServerSideProps({ query }) {
     const fileSafe = fileSafeProps(file);
 
     let props;
-    if (file) { // fichier trouvé
+    if (file) {
         props = { file: JSON.parse(JSON.stringify(fileSafe)), fid };
-    } else { // fichier non trouvé
+    } else {
         props = { file: null, fid, error: `Impossible de trouver le fichier ${fid}` };
     }
 
