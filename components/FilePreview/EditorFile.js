ed3e19e96c25e80f14db7ecd1ae0e21cec99ff5a
Sonny <sonnyasdev@gmail.com>
2021-10-20 14:26:19 +0200

    fix dependencies file editor + remove useless stuff

--- components/FilePreview/EditorFile.js
+++ components/FilePreview/EditorFile.js
@@ -1,30 +1,22 @@
 import { useEffect, useState } from 'react';
-import Editor, { useMonaco } from '@monaco-editor/react';
+import Editor from '@monaco-editor/react';
 
 export default function EditorFile({ file, language }) {
     const { url } = file;
     const [content, setContent] = useState(null);
-    const monaco = useMonaco();
 
     useEffect(() => {
         (async () => {
             const contentRequest = await fetch(url).then(response => response.text());
             setContent(contentRequest);
         })();
-    }, []);
-    
-    useEffect(() => {
-        if (monaco) {
-            console.log('here is the monaco isntance:', monaco);
-        }
-    }, [monaco]);
+        return () => setContent(null);
+    }, [url]);
 
     console.log('language', language);
     if (content) {
         return <Editor defaultLanguage={language} theme='vs-dark' defaultValue={content} className='code-editor' />
     } else {
-        return <>
-            Chargement du fichier en cours
-        </>;
+        return <p>Chargement du fichier en cours</p>;
     }
 }
