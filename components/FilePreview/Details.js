ed7f8d6d22b316631af55c7c461250e02b46a4bf
Sonny <sonnyasdev@gmail.com>
2021-10-28 05:18:21 +0200

    fix error dayjs date not synchronised with server

--- components/FilePreview/Details.js
+++ components/FilePreview/Details.js
@@ -1,4 +1,7 @@
 import dayjs from 'dayjs';
+import 'dayjs/locale/fr';
+dayjs.extend(require('dayjs/plugin/relativeTime'));
+dayjs.locale('fr');
 
 export default function Details({ file, mime }) {
     const { name, fileExtension, size, createdAt } = file;
