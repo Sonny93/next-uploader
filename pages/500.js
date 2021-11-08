bd8b31f29178524a5fa82756869c287c6f609f4a
Sonny <sonny.lepapa@gmail.com>
2021-11-08 09:10:24 +0100

    ajout pages custom 404 & 500 + ajout error boundary

---
+++ pages/500.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Custom404() {
+    return (<>
+        <div className='App error-page'>
+            <p>
+                <span>500</span> Une erreur est survenue sur le serveur
+            </p>
+            <Link href='/'>
+                <a>Revenir à l'accueil</a>
+            </Link>
+        </div>
+    </>)
+}
