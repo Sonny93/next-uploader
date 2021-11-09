783324555221c1da5527c73a6ffacc1ceb75876f
Sonny <sonnyasdev@gmail.com>
2021-11-09 09:46:22 +0100

    split filter component into dedicated file + ajout size fichiers calculés après filtrage

--- components/FilesList/FilesList.js
+++ components/FilesList/FilesList.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
 import File from './File';
-import { calculSize } from '../../utils';
+import Filter from './Filter';
 
 export default function FilesList({ files, showFilter, globalSize }) {
     const [inputContent, setInputContent] = useState('');
     const [filesFilter, setFilesFilter] = useState(files);
 
     return (<>
-        {showFilter && <div className='filter'>
-            <label htmlFor='input_search'>Rechercher • {globalSize && calculSize(globalSize)}</label>
-            <input
-                name='input_search'
-                id='input_search'
-                placeholder={`Rechercher parmis ${files?.length || 0} fichier${files?.length > 1 ? 's' : ''}`}
-                onChange={({ target }) => {
-                    setInputContent(target.value);
-                    if (!files || files?.length < 1) return;
-
-                    const filesFiltered = files.filter((file) => file.name.toLowerCase().includes(target.value.toLowerCase()) ? file : false);
-                    setFilesFilter(filesFiltered);
-                }}
-                value={inputContent} />
-        </div>}
+        {showFilter && 
+            <Filter 
+                files={files} 
+                filesFilter={filesFilter} 
+                globalSize={globalSize} 
+                inputContent={inputContent}
+                setInputContent={setInputContent} 
+                setFilesFilter={setFilesFilter} 
+            />}
         {filesFilter.length < 1 ? 
             <div className='no-files'>
                 <p>Aucune correspondance pour "<b>{inputContent}</b>"</p>
---
+++ components/FilesList/Filter.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { calculSize } from '../../utils';
+
+export default function Filter({ globalSize, files, filesFilter, inputContent, setFilesFilter, setInputContent }) {
+    const [filteredSize, setFilteredSize] = useState(0);
+
+    useEffect(() => {
+        if (filesFilter.length < 1) {
+            return setFilteredSize(0);
+        }
+
+        const size = filesFilter
+            .map((file) => parseInt(file.fileBrutSize, 10))
+            .reduce((prev, next) => prev + next);
+            
+        setFilteredSize(size);
+    }, [filesFilter, setFilesFilter]);
+
+    return (
+        <div className='filter'>
+            <label htmlFor='input_search'>Rechercher • {calculSize(filteredSize)} / {globalSize && calculSize(globalSize)}</label>
+            <input
+                name='input_search'
+                id='input_search'
+                placeholder={`Rechercher parmis ${files?.length || 0} fichier${files?.length > 1 ? 's' : ''}`}
+                onChange={({ target }) => {
+                    setInputContent(target.value);
+                    if (!files || files?.length < 1) return;
+
+                    const filesFiltered = files.filter((file) => file.name.toLowerCase().includes(target.value.toLowerCase()) ? file : false);
+                    setFilesFilter(filesFiltered);
+                }}
+                value={inputContent} />
+        </div>
+    );
+}
