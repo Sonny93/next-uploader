e1f381dcf7da1cc09fc8c145feaf934d6f9f46af
Sonny <sonny.lepapa@gmail.com>
2021-11-09 10:59:50 +0100

    ajout pagination fichiers accueil + changement couleur date fichier filelist accueil

--- components/FilesList/FilesList.js
+++ components/FilesList/FilesList.js
@@ -1,27 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
+
 import File from './File';
 import Filter from './Filter';
 
 export default function FilesList({ files, showFilter, globalSize }) {
+    const itemsPerPage = 20;
     const [inputContent, setInputContent] = useState('');
     const [filesFilter, setFilesFilter] = useState(files);
 
+    const [currentItems, setCurrentItems] = useState(filesFilter);
+    const [pageCount, setPageCount] = useState(0);
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+
+    useEffect(() => {
+        // Fetch items from another resources.
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(filesFilter.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(filesFilter.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage]);
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % filesFilter.length;
+        console.log(
+            `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
     return (<>
-        {showFilter && 
-            <Filter 
-                files={files} 
-                filesFilter={filesFilter} 
-                globalSize={globalSize} 
+        {showFilter &&
+            <Filter
+                files={files}
+                filesFilter={filesFilter}
+                globalSize={globalSize}
                 inputContent={inputContent}
-                setInputContent={setInputContent} 
-                setFilesFilter={setFilesFilter} 
+                setInputContent={setInputContent}
+                setFilesFilter={setFilesFilter}
             />}
-        {filesFilter.length < 1 ? 
+        {filesFilter.length < 1 ?
             <div className='no-files'>
                 <p>Aucune correspondance pour "<b>{inputContent}</b>"</p>
-            </div> : 
-            <ul className='filelist'>
-                {filesFilter.map((file, key) => <File file={file} key={key} index={key} />)}
-            </ul>}
+            </div> : <>
+                <ul className='filelist'>
+                    {currentItems.map((file, key) => <File file={file} key={key} index={key} />)}
+                </ul>
+                <ReactPaginate
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    breakLabel='...'
+                    nextLabel='Suivant'
+                    previousLabel='Précédent'
+                    renderOnZeroPageCount={null}
+                    containerClassName='controls-page'
+                    nextClassName='reset btn'
+                    previousClassName='reset btn'
+                    activeClassName='reset btn'
+                />
+            </>}
     </>);
 }
