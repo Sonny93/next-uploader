9bbf7cf7fe121647ed022b3b396a5419822714ee
Sonny <sonny.lepapa@gmail.com>
2021-11-10 15:19:29 +0100

    ajout bouton supprimer fichier via menu contextuel + endpoint api

--- components/FilesList/File.js
+++ components/FilesList/File.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import dayjs from 'dayjs';
+import toastr from 'toastr';
 
 import { AiOutlineFileImage, AiOutlineVideoCamera } from 'react-icons/ai';
 import { BiFile } from 'react-icons/bi';
@@ -16,7 +17,7 @@ const transitionStyles = {
     exited: { opacity: 0 }
 };
 
-export default function File({ file, index }) {
+export default function File({ file, index, contextMenu = false, setContextMenu, removeFile }) {
     const { file_id, name, size, fileMimeType, createdAt } = file;
     const mime = fileMimeType.split('/');
 
@@ -42,6 +43,27 @@ export default function File({ file, index }) {
         }
     }, [setIcon]);
 
+    function handleContextMenu(event) {
+        event.preventDefault();
+        console.log('context menu');
+        setContextMenu((prev) => prev === file_id ? null : file_id);
+    }
+
+    async function handleRemove() {
+        try {
+            const req = await fetch(`/api/file/${file_id}`, { method: 'DELETE' });
+            const data = await req.json();
+            if (!data?.ok)
+                toastr.error(data?.error);
+
+            toastr.success(data?.message);
+            removeFile(file);
+        } catch (error) {
+            toastr.error(error);
+            console.error(error);
+        }
+    }
+
     return (
         <Transition in={!!icon} timeout={0}>
             {state => (<>
@@ -49,22 +71,26 @@ export default function File({ file, index }) {
                     opacity: 0,
                     transition: `${index * 75}ms, transform .05s, box-shadow .05s, border .05s`,
                     ...transitionStyles[state]
-                 }}>
-                    <Link href={`/file/${file_id}`}>
-                        <a>
-                            <div className='icon-btn'>
-                                {icon}
-                            </div>
-                            <div className='meta'>
-                                <span className='name'>
-                                    {name}
-                                </span>
-                                <span className='details'>
-                                    <span style={{ color: '#3f88c5', fontWeight: '600' }}>{size}</span> - <span style={{ color: '#b3b3b3' }}>{dayjs(createdAt).format('D MMMM YYYY à HH:mm')}</span>
-                                </span>
-                            </div>
-                        </a>
-                    </Link>
+                 }} onContextMenu={handleContextMenu}>
+                    {contextMenu ? 
+                        <div>
+                            <button onClick={handleRemove}>Supprimer</button>
+                        </div> :
+                        <Link href={`/file/${file_id}`}>
+                            <a>
+                                <div className='icon-btn'>
+                                    {icon}
+                                </div>
+                                <div className='meta'>
+                                    <span className='name'>
+                                        {name}
+                                    </span>
+                                    <span className='details'>
+                                        <span style={{ color: '#3f88c5', fontWeight: '600' }}>{size}</span> - <span style={{ color: '#b3b3b3' }}>{dayjs(createdAt).format('D MMMM YYYY à HH:mm')}</span>
+                                    </span>
+                                </div>
+                            </a>
+                        </Link>}
                 </li>
             </>)}
 
