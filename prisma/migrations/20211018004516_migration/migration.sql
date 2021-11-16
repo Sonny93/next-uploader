a51f7e74a73fdcbab3ba26ef2ad7ba0ae2a42548
Sonny <sonnyasdev@gmail.com>
2021-11-16 13:03:19 +0100

    remove migrations + add models log_connection & log_http

--- prisma/migrations/20211018004516_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[file_id]` on the table `File` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `File_file_id_key` ON `File`(`file_id`);
