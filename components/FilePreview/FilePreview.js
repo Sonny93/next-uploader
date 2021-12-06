0a1f8226f96f392f9c8fdad884f0ba539ac5dd45
Sonny <sonny.lepapa@gmail.com>
2021-12-06 23:46:44 +0100

    fix: hide music recognition component si ce n'est pas une vidéo ou un audio

--- components/FilePreview/FilePreview.js
+++ components/FilePreview/FilePreview.js
@@ -17,7 +17,7 @@ export default function FilePreview({ file, music_recognition }) {
     const [content, setContent] = useState(null);
     const [loadingContent, setLoading] = useState(false);
     const mime = fileMimeType.split('/');
-    
+
     useEffect(() => {
         (async () => {
             if (mime[0] === 'image') {
@@ -37,27 +37,45 @@ export default function FilePreview({ file, music_recognition }) {
         })();
     }, [setLoading, contentRef, file, fileExtension, name, url]);
 
-    return <>
-        <div className={styles['preview-wrapper']}>
-            {loadingContent && <Loader top={true} backdrop={true} />}
-            {content}
-        </div>
-        <Details file={file} mime={mime} />
-        {music_recognition ? 
-            <SongRecognition music_recognition={music_recognition} /> :
-            <p className={styles['no_music']}>Aucune musique n'a été détectée</p>}
-    </>;
+    if (music_recognition && (mime[0] === 'video' || mime[0] === 'audio')) {
+        return (<>
+            <div className={styles['preview-wrapper']}>
+                {loadingContent && <Loader top={true} backdrop={true} />}
+                {content}
+            </div>
+            <Details file={file} mime={mime} />
+            <SongRecognition music_recognition={music_recognition} />
+        </>);
+    } else if (!music_recognition && (mime[0] === 'video' || mime[0] === 'audio')) {
+        return (<>
+            <div className={styles['preview-wrapper']}>
+                {loadingContent && <Loader top={true} backdrop={true} />}
+                {content}
+            </div>
+            <Details file={file} mime={mime} />
+            <p>Aucune musique détectée</p>
+        </>);
+    } else {
+
+        return (<>
+            <div className={styles['preview-wrapper']}>
+                {loadingContent && <Loader top={true} backdrop={true} />}
+                {content}
+            </div>
+            <Details file={file} mime={mime} />
+        </>);
+    }
 }
 
 const languages = [
-    { key: 'cs', value: 'csharp' }, 
-    { key: 'json' }, 
-    { key: 'plain' }, 
-    { key: 'js', value: 'javascript' }, 
-    { key: 'html' }, 
-    { key: 'css' }, 
-    { key: 'txt', value: 'plain' }, 
-    { key: 'md', value: 'markdown' }, 
-    { key: 'xml' }, 
+    { key: 'cs', value: 'csharp' },
+    { key: 'json' },
+    { key: 'plain' },
+    { key: 'js', value: 'javascript' },
+    { key: 'html' },
+    { key: 'css' },
+    { key: 'txt', value: 'plain' },
+    { key: 'md', value: 'markdown' },
+    { key: 'xml' },
     { key: 'lock' }
 ];
