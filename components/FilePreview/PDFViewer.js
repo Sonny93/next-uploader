296502167783b6aa56a30c98c8fd654ed9f7f526
Sonny <sonnyasdev@gmail.com>
2021-12-12 06:18:14 +0100

    Ajout d'un title pour l'iframe pdf viewer

--- components/FilePreview/PDFViewer.js
+++ components/FilePreview/PDFViewer.js
@@ -5,7 +5,7 @@ import styles from '../../styles/file-preview/file-preview.module.scss';
 export default function PDFViewer({ src }) {
     return (<>
         <div className={styles['preview-wrapper']}>
-            <iframe style={{ flex: '1', width: '100%' }} src={src} />
+            <iframe style={{ flex: '1', width: '100%' }} src={src} title={`PDF : ${src}`} />
         </div>
     </>);
 }
