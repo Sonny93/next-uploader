bf4a1d103374ab68f452e6ceecbe351891d144c3
Sonny <sonny.lepapa@gmail.com>
2021-12-12 06:20:30 +0100

    suppression bordure iframe pdf viewer

--- components/FilePreview/PDFViewer.js
+++ components/FilePreview/PDFViewer.js
@@ -5,7 +5,7 @@ import styles from '../../styles/file-preview/file-preview.module.scss';
 export default function PDFViewer({ src }) {
     return (<>
         <div className={styles['preview-wrapper']}>
-            <iframe style={{ flex: '1', width: '100%' }} src={src} title={`PDF : ${src}`} />
+            <iframe style={{ flex: '1', width: '100%' }} src={src} frameborder='0' title={`PDF : ${src}`} />
         </div>
     </>);
 }
