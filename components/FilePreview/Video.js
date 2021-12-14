0ed06a7abccec63eadf6abee68cb7b289e384795
Sonny <sonnyasdev@gmail.com>
2021-12-14 16:02:01 +0100

    Autoplay désactivé + redesign léger du component Filter + suppression arrière plan footter

--- components/FilePreview/Video.js
+++ components/FilePreview/Video.js
@@ -4,7 +4,7 @@ import styles from '../../styles/file-preview/file-preview.module.scss';
 export default function Video({ src, music_recognition }) {
     return (<>
         <div className={styles['preview-wrapper']}>
-            <video src={src} controls autoPlay loop />
+            <video src={src} controls autoPlay={false} loop />
         </div>
         {music_recognition ? 
             <SongRecognition music_recognition={music_recognition} /> :
