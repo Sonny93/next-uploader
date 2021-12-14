696f0b479874dd554508c48b3ac677a17199347e
Sonny <sonny.lepapa@gmail.com>
2021-12-14 09:55:22 +0100

    Ajout avatar menu + espace entre items pages et admin/auth

--- components/MenuNavigation/MenuNavigationAdmin.js
+++ components/MenuNavigation/MenuNavigationAdmin.js
@@ -6,13 +6,9 @@ import styles from '../../styles/menu.module.scss';
 
 export default function MenuNavigationAdmin() {
     const session = useSession();
+    console.log(session);
     return (
         <MenuNavigation session={session}>
-            <li className={styles['item']}>
-                <Link href='/admin'>
-                    <a>Dashboard</a>
-                </Link>
-            </li>
             <li className={styles['item']}>
                 <Link href='/admin/logs_admin/http'>
                     <a>Logs HTTP</a>
