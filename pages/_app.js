c26323cc9f4dc35f33cf4de55684310ae4c0ee05
Sonny <sonny.lepapa@gmail.com>
2021-12-15 10:32:37 +0100

    Fix: 'Aucune correspondance pour ""' affiché lorsque les fichiers sont supprimés

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import { Provider } from 'next-auth/client';
+import { useState } from 'react';
 
 import 'toastr/build/toastr.css';
 import '../styles/globals.scss';
 
 import PageLoader from '../components/Loader/Page';
 import ErrorBoundary from '../components/ErrorBoundary';
-import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
 	const [isTransitioning, setTransitioning] = useState(null);
 
+	const transitionClass = isTransitioning === null ? '' : !isTransitioning ? 'transition-class-in' : 'transition-class-out';
 	return (
 		<ErrorBoundary>
 			<Provider session={pageProps.session} >
 				<PageLoader setTransitioning={setTransitioning} />
-				<Component transitionClass={isTransitioning === null ? '' : !isTransitioning ? 'transition-class-in' : 'transition-class-out' } {...pageProps} />
+				<Component transitionClass={transitionClass} {...pageProps} />
 			</Provider>
 		</ErrorBoundary>
 	);
